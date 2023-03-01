5bdbd87f05b0f00adebdf7da43241e2b5ed2247c
Igor Kononenko <igorkono343@gmail.com>
2023-03-01 18:35:01 +0200

    deploying with ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// base: '/vue-calculator/',
+	base: '/vue-calculator/',
 	server: {
 		port: 3000
 	},
