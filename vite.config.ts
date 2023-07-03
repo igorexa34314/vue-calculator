a774a538f9a9d2454c0b3bb08b2a53a511004620
Igor Kononenko <110349044+igorexa34314@users.noreply.github.com>
2023-07-03 20:05:49 +0300

    Update static.yml

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
 	return defineConfig({
-		base: process.env.VITE_BASE_URL || '/',
+		base: process.env.VITE_BASE || '/',
 		server: {
 			port: +process.env.VITE_PORT || 3000
 		},
